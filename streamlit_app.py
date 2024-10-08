b779c452507905b329abd2ae77b03bc6e748de71
fizautm <fizautm@gmail.com>
2024-10-08 21:44:54 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,10 @@ with st.expander('Data Visualization'):
 # Data Preparation
 with st.sidebar:
   st.header('Input features')
-  "","bill_depth_mm","flipper_length_mm", "body_mass_g"
+  #"","bill_depth_mm","flipper_length_mm", "body_mass_g"
   island = st.selectbox('Island', ('Biscoe', 'Dream', 'Torgersen'))
   gender = st.selectbox('Gender', ('male', 'female'))
   bill_length_mm = st.slider('Bill length (mm)', 32.1, 59.6, 43.9)
+  bill_ldepth_mm = st.slider('Bill depth (mm)', 13.1, 21.5, 17.2)
+  flipper_length_mm = st.slider('Flipper length (mm)', 172.0, 231.0, 201.0)
+  body_mass_g = st.slider('Body mass (g)', 2700.0, 6300.0, 4207.0)
