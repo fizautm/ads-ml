6bbaebfe4d99231decbbb7adebfc8be5062f3479
fizautm <fizautm@gmail.com>
2024-10-08 21:41:15 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,4 +27,4 @@ with st.sidebar:
   "","bill_depth_mm","flipper_length_mm", "body_mass_g"
   island = st.selectbox('Island', ('Biscoe', 'Dream', 'Torgersen'))
   gender = st.selectbox('Gender', ('male', 'female'))
-  bill_length_mm = st.slider('Bill length (mm)', 32.1, 59.6, 43.9))
+  bill_length_mm = st.slider('Bill length (mm)', 32.1, 59.6, 43.9)
