c3f5d1526e4cab1b70629298cf235c31df73c339
fizautm <fizautm@gmail.com>
2024-10-08 20:47:48 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,7 @@ st.title('🤖 Agile Data Science')
 
 st.write('This is app builds a machine learning model')
 
+with st.expander('Data'):
+  st.write('**Raw Data**')
 df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
 df
