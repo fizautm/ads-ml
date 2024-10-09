0edf61e25247cf02516724ba2b6b885e815c0d53
fizautm <fizautm@gmail.com>
2024-10-09 13:05:25 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -81,7 +81,7 @@ prediction = clf.predict(input_row)
 prediction_proba = clf.predict_proba(input_row)
 
 df_prediction_proba = pd.DataFrame(prediction_proba)
-prediction_proba.column = ['Adelie', 'Chinstrap', 'Gentoo']
+df_prediction_proba.column = ['Adelie', 'Chinstrap', 'Gentoo']
 #prediction_proba.rename(columns={0: 'Adelie',
 #                                1: 'Chinstrap',
  #                               2: 'Gentoo'})
