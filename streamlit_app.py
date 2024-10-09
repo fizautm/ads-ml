b49a8d64ea41349a468c93a7b1a321aa612a92dc
fizautm <fizautm@gmail.com>
2024-10-09 13:00:20 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -79,5 +79,5 @@ clf.fit(x, y)
 #Apply model to make predictions
 prediction = clf.predict(input_row)
 prediction_proba = clf.predict_proba(input_row)
-prediction_proba
+prediction_proba.column = ['Adelie', 'Chinstrap', 'Gentoo']
 
